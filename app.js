954ebcdd8cf52b8c1ff3203c608e531b5e69939d
GreggtheDev <greggthedev@gmail.com>
2024-07-03 20:41:19 -0400

    middleware to serve static files

--- app.js
+++ app.js
@@ -12,3 +12,6 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse incoming JSON requests and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
