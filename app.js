7e549bd09bd4305fc82e3fbefae18df24a2da407
GreggtheDev <greggthedev@gmail.com>
2024-07-03 20:38:48 -0400

    definied port 3000

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ const path = require('path');
 
 // Create an instance of an Express application
 const app = express();
+
+// Define the port number to listen on, using the environment variable or defaulting to 3000
+const PORT = process.env.PORT || 3000;
